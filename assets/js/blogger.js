2648930ca5a8be73775db6c9ed4c0c7b9a13a4e3
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-06 16:49:23 -0500

    1649

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -5,7 +5,7 @@ function getposts(){
 				output+="<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>";
 			}
 			document.getElementById("blog").innerHTML=output; 
-	 }
+	 });
 }
 
 /* 	
