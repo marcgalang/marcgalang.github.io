6d5275990042df9aa89e80a8e61aaead43eae2ee
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-06 16:48:18 -0500

    1648

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,16 +1,15 @@
 function getposts(){
-	console.log("getposts function");
+	 $.getJSON('posts.json', function(data){
+		var output="";
+			for (var i in data.entries){ 
+				output+="<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>";
+			}
+			document.getElementById("blog").innerHTML=output; 
+	 }
 }
 
 /* 	
 			var data = getposts();
 	  
-			var output="";
-			for (var i in data.entries){
 			
-			<!--     replace as needed below so it can create a big html string   
-			output+="<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>";
-			
-			}
-			document.getElementById("blog").innerHTML=output;
 	 */
