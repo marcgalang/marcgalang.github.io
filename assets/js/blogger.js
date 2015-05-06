3e7dff4ea29bfa400de8aff147f0ce63af0edb0b
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-06 16:50:19 -0500

    1650

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,5 +1,5 @@
 function getposts(){
-	 $.getJSON('posts.json', function(data){
+	 $.getJSON('entries.json', function(data){
 		var output="";
 			for (var i in data.entries){ 
 				output+="<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>";
