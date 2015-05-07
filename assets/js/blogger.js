2ebbb7d31324504cf747a2883fa5139d8085891f
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 17:54:44 -0500

    1754

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,17 +2,20 @@
 
 function getposts(){
 	console.log("getposts running...");
-	$("#left").hide();
-	console.log("hiding left button");
-	$("#right").hide();
-	console.log("hiding right button");
-	$("#blog").hide();
-	console.log("hiding blog");
-	$.getJSON('entries.json', function(data){
-		for (var i in data.entries){ 
-			$("#blog").append("<div id='"+i+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
-			}
-	 });
+	console.log($("#blog").childNodes.length);
+	if ($("#blog").childNodes.length<2){
+		$("#left").hide();
+		console.log("hiding left button");
+		$("#right").hide();
+		console.log("hiding right button");
+		$("#blog").hide();
+		console.log("hiding blog");
+		$.getJSON('entries.json', function(data){
+			for (var i in data.entries){ 
+				$("#blog").append("<div id='"+i+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
+				}
+		 });
+	}	 
 }
 
 function pager(change){
