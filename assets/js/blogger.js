ac8277cba62e58103c04512d0ffeeeb409a57069
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 17:41:43 -0500

    1741

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,8 +2,11 @@
 
 function getposts(){
 	$("#left").hide();
+	console.log("hiding left button");
 	$("#right").hide();
+	console.log("hiding right button");
 	$("#blog").hide();
+	console.log("hiding blog");
 	$.getJSON('entries.json', function(data){
 		for (var i in data.entries){ 
 			$("#blog").append("<div id='"+i+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
@@ -19,7 +22,7 @@ function pager(change){
 	}
 	for (i=0;i<=4;i++){
 		$(posts[page*5+i]).show();
-		console.log("we should show post #"+page*5+i)
+		console.log("we should show post #"+page*5);
 	}
 	if (page ==0){
 		$("#left").hide();
@@ -35,6 +38,7 @@ function pager(change){
 
 function displayBlog(){
 	$("#blog").show();
+	console.log("show blog");
 	pager(0);
 	
 	
