ce25b27402f7148f1521c920c9469fb5cd2119cd
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 16:08:29 -0500

    1608

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,6 +1,9 @@
-var page=0
+
 
 function getposts(){
+	$("#left").hide();
+	$("#right").hide();
+	$("#blog").hide();
 	$.getJSON('entries.json', function(data){
 		for (var i in data.entries){ 
 			$("#blog").append("<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
@@ -10,5 +13,25 @@ function getposts(){
 
 function pager(change){
 	page+=change;
+	var posts=$("#blog").childNodes;
+	for (post in posts){post.hide();}
+	for (i=0;i<=4,i++){
+		posts[page*5+i].show();
+	};
+	if page ==0 {
+		$("#left").hide();
+	}else{
+		$("#left").show();
+	}
+	if page ==ceil(c.length/5) {
+		$("#right").hide();
+	}else{
+		$("#right").show();
+	}
+}
+
+function displayBlog(){
+	pager(0);
+	
 	
 }
