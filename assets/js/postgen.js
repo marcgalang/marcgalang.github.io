50de8c6467ea6ec81c70a4a9be9ffed1e599b77b
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 11:09:42 -0500

    1109

--- assets/js/postgen.js
+++ assets/js/postgen.js
@@ -36,7 +36,7 @@ function getdate(){
 function post(){
 	$("#json").append('{"entries":['  + '<br />');
 	$.getJSON('entries.json', function(data) {
-		var reps={'<br>':'&lt;br&gt;'};
+		var reps={'<br>':'&lt;br&gt;','&#91':'&amp;#91'};
 		$.each(data.entries, function(key, val) {
 			$.each(reps, function(k,v){
 				var re = new RegExp(k,"g");
