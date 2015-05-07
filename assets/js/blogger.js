ee5901e52fd2b5f081e9a9f00b8164b0c9423f35
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 13:54:46 -0500

    no message

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -3,7 +3,7 @@ var page=0
 function getposts(){
 	$.getJSON('entries.json', function(data){
 		for (var i in data.entries){ 
-			$("blog").append("<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
+			$("#blog").append("<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
 			}
 	 });
 }
