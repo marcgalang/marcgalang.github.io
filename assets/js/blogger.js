b03f8458bd3dd5cafbaf533e5d561f7baf1e0b20
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-07 13:50:35 -0500

    updating w/jquery

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,15 +1,14 @@
+var page=0
+
 function getposts(){
-	 $.getJSON('entries.json', function(data){
-		var output="";
-			for (var i in data.entries){ 
-				output+="<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>";
+	$.getJSON('entries.json', function(data){
+		for (var i in data.entries){ 
+			$("blog").append("<div id="+i+" class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
 			}
-			document.getElementById("blog").innerHTML=output; 
 	 });
 }
 
-/* 	
-			var data = getposts();
-	  
-			
-	 */
+function pager(change){
+	page+=change;
+	
+}
