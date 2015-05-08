05f4e8b2768f7b7c9ccbfa0bb2f6834b65f08bcf
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-08 17:42:50 -0500

    1742

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,6 +2,7 @@ function getposts(){
 	$.getJSON('entries.json', function(data){
 		$.each(data.entries, function(key, val) { 
 			$("#blog").append("<div id='"+key+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[key].date+"</p></small><h5>"+data.entries[key].title+"</h5><p>"+data.entries[key].text+"</p><hr/></div>");
+		
 			if (data.entries.length-1==key){
 				pager(0);
 			};
@@ -15,14 +16,11 @@ function pager(change){
 	page+=change;
 	$("#pg").html(page);
 	var count = $('div.blogpost').length;
-	console.log('blogposts now ='+count);
  	for (c=0;c<=count;c++){
 		$("#"+c+"").hide();
-		console.log("hiding id"+"#"+c+""); 
 	}
 	for (i=-5;i<=-1;i++){
-		$("#"+[page*5+i]+"").show(200);
-		console.log("showing #"+(page*5+i)); 
+		$("#"+[page*5+i]+"").show(400);
 	}
 	if (page ==1){
 		$("#left").hide();
@@ -37,5 +35,18 @@ function pager(change){
 	return false;
 }
 
-
+function getselection(){
+	console.log("running 'get selection'");
+	if ($("search2").val()=null){
+		console.log("it's empty");
+		return false;
+	}
+	
+	var s2 = new RegExp($("search2").val(),"i")
+	console.log('looking for "'+s2+'"');
+	var count = $('div.blogpost').length;
+	for (c=0;c<=count;c++){
+	console.log($("#"+c+""));
+	}
+}
 	
