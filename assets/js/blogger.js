11753888b4ca7ea2ac1c343a10f8fe44c4f98d41
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-08 14:15:07 -0500

    1415

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -5,7 +5,9 @@ function getposts(){
 		$.each(data.entries, function(key, val) { 
 			$("#blog").append("<div id='"+key+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[key].date+"</p></small><h5>"+data.entries[key].title+"</h5><p>"+data.entries[key].text+"</p><hr/></div>");
 			console.log("getting json "+key);
-			});
+		}.fadeIn(5,function(){
+			console.log($("div.blogpost").length);
+		}));	
 	 });
 	 	return false;
 	}	 
