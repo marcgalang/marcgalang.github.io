8add62275c20eab6243c33336a3bed68c5c397d9
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-08 09:22:42 -0500

    0922

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -14,8 +14,8 @@ function getposts(){
 				$("#blog").append("<div id='"+i+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
 				}
 		 });
-			$("#blog").hide();
-			console.log("hiding blog");
+		/* 	$("#blog").hide();
+			console.log("hiding blog"); */
 		}	 
 }
 
