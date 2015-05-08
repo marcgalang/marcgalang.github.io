4ebac326c8ee8dfa063f8f7d99620c16eac31910
unknown <MGalang_H@Dev04.bsecode.inc>
2015-05-08 09:39:38 -0500

    0939

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -12,9 +12,10 @@ function getposts(){
 		$.getJSON('entries.json', function(data){
 			for (var i in data.entries){ 
 				$("#blog").append("<div id='"+i+"' class='col-sm-12 blogpost'><small><p class='muted' style='float:right;'>"+data.entries[i].date+"</p></small><h5>"+data.entries[i].title+"</h5><p>"+data.entries[i].text+"</p><hr/></div>");	
+				console.log($('div.blogpost').length);
 				}
 		 });
-		 console.log($('div.blogpost').length);
+		 
 		/* 	$("#blog").hide();
 			console.log("hiding blog"); */
 		}	 
